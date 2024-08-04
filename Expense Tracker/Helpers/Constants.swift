0ecea7c5c0bed5e77811dc70b63dbe305db38ff8
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-04 19:19:32 +0530

    added intro title and button on intro screen with ui

---
+++ Expense Tracker/Helpers/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Expense Tracker
+//
+//  Created by Vikrant Vani on 04/08/24.
+//
+
+import SwiftUI
+
+let appTint: Color = .purple
--- Expense Tracker/View/IntroScreen.swift
+++ Expense Tracker/View/IntroScreen.swift
@@ -19,16 +19,46 @@ struct IntroScreen: View {
          
             //Points View
             VStack(alignment: .leading, spacing: 25, content: {
+                PointView(symbol: "dollarsign", title: "Transactions", subTitle: "Keep Track Of Your Earnings and Expenses.")
+                
+                PointView(symbol: "chart.bar.fill", title: "Visual Charts", subTitle: "View Your Transactions Using Eye Catching Graphic Representations.")
+                
+                PointView(symbol: "magnifyingglass", title: "Advance Filters", subTitle: "Find The Expense You Want By Advance Search And Filtering.")
                 
             })
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 15)
+            
+            Spacer(minLength: 10)
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Continue")
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 14)
+                    .background(appTint.gradient, in: .rect(cornerRadius: 12))
+                    .contentShape(.rect)
+            })
         }
+        .padding(15)
     }
     @ViewBuilder
     func PointView(symbol: String, title: String, subTitle: String) -> some View{
-        HStack(spacing: 15){
+        HStack(spacing: 20){
             Image(systemName: symbol)
                 .font(.largeTitle)
+                .foregroundStyle(appTint.gradient)
+                .frame(width: 45)
             
+            VStack(alignment: .leading, spacing: 6, content: {
+                Text(title)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                
+                Text(subTitle)
+                    .foregroundStyle(.gray)
+            })
         }
     }
 }
