6b13c0953a0d0608eb1f57b786f8dceb5fd1715b
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-04 22:29:42 +0530

    added arrow images near expenses

--- Expense Tracker/View/Helpers/CardView.swift
+++ Expense Tracker/View/Helpers/CardView.swift
@@ -29,8 +29,29 @@ struct CardView: View {
                 HStack(spacing: 0){
                     ForEach(Category.allCases, id: \.rawValue){ category in
                         let symbolImage = category == .income ? "arrow.down" : "arrow.up"
+                        let tint = category == .income ? Color.green : Color.red
                         HStack(spacing: 10){
-                            
+                            Image(systemName: symbolImage)
+                                .font(.callout.bold())
+                                .foregroundStyle(tint)
+                                .frame(width: 35, height: 35)
+                                .background(){
+                                    Circle()
+                                        .fill(tint.opacity(0.25).gradient)
+                                }
+                            VStack(alignment: .leading, spacing: 4){
+                                Text(category.rawValue)
+                                    .font(.caption2)
+                                    .foregroundStyle(.gray)
+                                
+                                Text(currencyString(category == .income ? income : expense, allowedDigits: 0))
+                                    .font(.callout)
+                                    .fontWeight(.semibold)
+                                    .foregroundStyle(.primary)
+                            }
+                            if category == .income{
+                                Spacer(minLength: 10)
+                            }
                         }
                     }
                 }
