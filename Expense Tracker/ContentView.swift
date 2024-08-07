4cf76673d2c21798b6084f179288558eed1237b7
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-07 19:23:16 +0530

    added biometrics access to settings buttons

--- Expense Tracker/ContentView.swift
+++ Expense Tracker/ContentView.swift
@@ -8,33 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    //intro visibility status
     @AppStorage("isFirsTime") private var isFirstTime: Bool = true
+    
+    //App lock properties
+    @AppStorage("isAppLockEnabled") private var isAppLockEnabled: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
+    
+    //active tab
     @State private var activeTab: Tab = .recents
     
     var body: some View {
-        TabView(selection: $activeTab){
-            Recents()
-                .tag(Tab.recents)
-                .tabItem { Tab.recents.tabContent }
+        LockView(lockType: .biometric, lockPin: "", isEnabled: isAppLockEnabled, lockWhenAppGoesBackground: lockWhenAppGoesBackground){
             
-            Search()
-                .tag(Tab.search)
-                .tabItem { Tab.search.tabContent }
-            
-            Graphs()
-                .tag(Tab.charts)
-                .tabItem { Tab.charts.tabContent }
-            
-            Settings()
-                .tag(Tab.settings)
-                .tabItem { Tab.settings.tabContent }
+            TabView(selection: $activeTab){
+                Recents()
+                    .tag(Tab.recents)
+                    .tabItem { Tab.recents.tabContent }
+                
+                Search()
+                    .tag(Tab.search)
+                    .tabItem { Tab.search.tabContent }
+                
+                Graphs()
+                    .tag(Tab.charts)
+                    .tabItem { Tab.charts.tabContent }
+                
+                Settings()
+                    .tag(Tab.settings)
+                    .tabItem { Tab.settings.tabContent }
+            }
+            .tint(appTint)
+            .sheet(isPresented: $isFirstTime, content: {
+                IntroScreen()
+                    .interactiveDismissDisabled()
+            })
         }
-        .tint(appTint)
-        .sheet(isPresented: $isFirstTime, content: {
-            IntroScreen()
-                .interactiveDismissDisabled()
-        })
     }
 }
 
