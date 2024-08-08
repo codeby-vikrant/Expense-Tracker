fb6f3dedd9db02f9d4d2f1f9ca8623d29d421f78
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-08 22:48:45 +0530

    changed the variables to another files

--- Expense Tracker/View/TabViews/Graphs.swift
+++ Expense Tracker/View/TabViews/Graphs.swift
@@ -9,16 +9,28 @@ import SwiftUI
 import Charts
 import SwiftData
 
-struct ChartGroup: Identifiable{
-    let id: UUID = .init()
-    var date: Date
-    var categories: [ChartCategory]
-    var totalIncome: Double
-    var totalExpense: Double
+struct Graphs: View {
+    
+    //View properties
+    @Query(animation: .snappy) private var transactions: [Transaction]
+    @State private var chartGroups: [ChartGroup] = []
+    
+    var body: some View {
+        ScrollView(.vertical){
+            LazyVStack(spacing: 10){
+                //Chart view
+                Chart{
+                    ForEach(chartGroups){ group in
+                        ForEach(group.categories){ chart in
+                            BarMark(x: .value("Month", format(date: group.date, format: "MM yy")), y: .value(chart.category.rawValue, chart.totalValue))
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
-struct ChartCategory: Identifiable{
-    let id: UUID = .init()
-    let totalValue: Double
-    let category: Category
+#Preview {
+    Graphs()
 }
