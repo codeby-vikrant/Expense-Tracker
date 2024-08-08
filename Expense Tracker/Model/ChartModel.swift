333396639539c21551716bbb2aa4f126f459a4a3
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-08 22:38:21 +0530

    implementing the graohs page

---
+++ Expense Tracker/Model/ChartModel.swift
@@ -0,0 +1,18 @@
+//
+//  ChartModel.swift
+//  Expense Tracker
+//
+//  Created by Vikrant Vani on 08/08/24.
+//
+
+import SwiftUI
+
+struct ChartModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartModel()
+}
