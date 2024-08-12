cad13005b1723b1ecb70de142cba27901d9be34a
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-12 12:09:45 +0530

    removed the emoji part from widgets

--- StatsCardView/StatsCardView.swift
+++ StatsCardView/StatsCardView.swift
@@ -10,24 +10,18 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+        SimpleEntry(date: Date())
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: Date())
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
+        
+        entries.append(.init(date: .now))
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
@@ -36,7 +30,6 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
 }
 
 struct StatsCardViewEntryView : View {
@@ -44,11 +37,7 @@ struct StatsCardViewEntryView : View {
 
     var body: some View {
         VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
 
-            Text("Emoji:")
-            Text(entry.emoji)
         }
     }
 }
@@ -67,6 +56,7 @@ struct StatsCardView: Widget {
                     .background()
             }
         }
+        .contentMarginsDisabled()
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
     }
@@ -75,6 +65,5 @@ struct StatsCardView: Widget {
 #Preview(as: .systemSmall) {
     StatsCardView()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+    SimpleEntry(date: .now)
 }
