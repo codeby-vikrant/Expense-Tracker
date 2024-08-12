f21e0b3b039008bfab02d9dbe62fa887bf827a55
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-12 12:06:39 +0530

    added a new widget extension

---
+++ StatsCardView/StatsCardViewBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StatsCardViewBundle.swift
+//  StatsCardView
+//
+//  Created by Vikrant Vani on 12/08/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StatsCardViewBundle: WidgetBundle {
+    var body: some Widget {
+        StatsCardView()
+    }
+}
